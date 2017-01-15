e6381a1208c1dd1637bf686f5f2028a43da58b54
kumada <s-kumada@nifty.com>
2017-01-15 16:01:58 +0900

    ...

--- util/check_images.py
+++ util/check_images.py
@@ -8,15 +8,23 @@ import sys
 DIR_PATH = "/Users/kumada/Data/image_net/images"
 
 def check_dir(dir_path):
+    c = 0
     for f in os.listdir(dir_path):
         fp = os.path.join(dir_path, f)
-        image = Image.open(fp)
-        print(image.mode) 
+        try: 
+            image = Image.open(fp)
+            c += 1
+        except IOError, e:
+            print(" {} is not image".format(fp))
+    return c
+
 
 if __name__ == "__main__":
     for root, dirs, files in os.walk(DIR_PATH):
         for d in dirs:
             dir_path = os.path.join(root, d)
-            check_dir(dir_path)
-            sys.exit()
+            count = check_dir(dir_path)
+            print("{a}:{c}".format(a=dir_path, c=count))
+
+
 
