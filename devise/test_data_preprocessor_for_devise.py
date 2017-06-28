d0ac710007da7c7a6944ac8ba33c0f3833eb5d2f
Seiya Kumada <s-kumada@nifty.com>
2017-06-28 14:02:53 +0000

    亀の歩み

--- devise/test_data_preprocessor_for_devise.py
+++ devise/test_data_preprocessor_for_devise.py
@@ -5,6 +5,7 @@ import unittest
 from data_preprocessor_for_devise import DataPreprocessorForDevise
 import os
 import numpy as np
+import chainer.cuda
 import sys
 sys.path.append('../visual')
 from modified_reference_caffenet import *  # noqa
@@ -44,64 +45,81 @@ class TestDataPreprocessorForDevise(unittest.TestCase):
             is_scaled=True
         )
 
-    def test_init(self):
-        self.assertTrue(TestDataPreprocessorForDevise.preprocessor is not None)
+    # def test_init(self):
+    #     self.assertTrue(TestDataPreprocessorForDevise.preprocessor is not None)
 
-    def test_len(self):
-        self.assertTrue(len(TestDataPreprocessorForDevise.preprocessor) == 49628)
+    # def test_len(self):
+    #     self.assertTrue(len(TestDataPreprocessorForDevise.preprocessor) == 49628)
 
-    def make_dummy_image(self):
-        # batch, channel, row, col
-        return np.random.randint(0, 256, (1, 3, 227, 227)).astype('f')
+    # def make_dummy_image(self):
+    #     # batch, channel, row, col
+    #     return np.random.randint(0, 256, (1, 3, 227, 227)).astype('f')
 
-    def make_dummy_image_2(self):
-        # batch, channel, row, col
-        return np.random.randint(0, 256, (3, 227, 227)).astype('f')
+    # def make_dummy_image_2(self):
+    #     # batch, channel, row, col
+    #     return np.random.randint(0, 256, (3, 227, 227)).astype('f')
 
-    def test_load_model(self):
-        model = TestDataPreprocessorForDevise.preprocessor.model
+    # def test_load_model(self):
+    #     model = TestDataPreprocessorForDevise.preprocessor.model
 
-        dummy_image = self.make_dummy_image()
-        dummy_image = chainer.cuda.to_gpu(dummy_image)
-        feature, softmax = model(dummy_image, None)
-        self.assertTrue(feature.shape == (1, 4096))
+    #     dummy_image = self.make_dummy_image()
+    #     dummy_image = chainer.cuda.to_gpu(dummy_image)
+    #     feature, softmax = model(dummy_image, None)
+    #     self.assertTrue(feature.shape == (1, 4096))
 
-        model_path = '/home/ubuntu/results/devise/20170517-07-20/model_iter_11453'
-        class_size = 99
-        original_model = ModifiedReferenceCaffeNet(class_size)
-        chainer.serializers.load_npz(model_path, original_model)
-        original_model.select_phase('predict')
-        original_model.to_gpu()
-        answer_softmax = original_model(dummy_image, None)
+    #     model_path = '/home/ubuntu/results/devise/20170517-07-20/model_iter_11453'
+    #     class_size = 99
+    #     original_model = ModifiedReferenceCaffeNet(class_size)
+    #     chainer.serializers.load_npz(model_path, original_model)
+    #     original_model.select_phase('predict')
+    #     original_model.to_gpu()
+    #     answer_softmax = original_model(dummy_image, None)
 
-        self.assertTrue(np.all(chainer.cuda.to_cpu(answer_softmax.data) == chainer.cuda.to_cpu(softmax.data)))
+    #     self.assertTrue(np.all(chainer.cuda.to_cpu(answer_softmax.data) == chainer.cuda.to_cpu(softmax.data)))
 
-    def test_load_word2vec_model(self):
-        word2vec_model_path = '/home/ubuntu/results/word2vec/word2vec.model'
-        (word2index,  word2vec_w) = DataPreprocessorForDevise.load_word2vec_model(word2vec_model_path)
-        self.assertTrue(word2index['wringer'] == 223093)
+    # def test_load_word2vec_model(self):
+    #     word2vec_model_path = '/home/ubuntu/results/word2vec/word2vec.model'
+    #     (word2index,  word2vec_w) = DataPreprocessorForDevise.load_word2vec_model(word2vec_model_path)
+    #     self.assertTrue(word2index['wringer'] == 223093)
 
-    def test_convert_to_feature(self):
-        dummy_image = self.make_dummy_image_2()
-        f = TestDataPreprocessorForDevise.preprocessor.convert_to_feature(dummy_image)
-        self.assertTrue(f.shape == (1, 4096))
+    # def test_convert_to_feature(self):
+    #     dummy_image = self.make_dummy_image_2()
+    #     f = TestDataPreprocessorForDevise.preprocessor.convert_to_feature(dummy_image)
+    #     self.assertTrue(f.shape == (1, 4096))
 
-    def test_convert_to_word_vector(self):
-        v = TestDataPreprocessorForDevise.preprocessor.convert_to_word_vector(98)
-        self.assertTrue(v.shape == (200,))
-        a = np.linalg.norm(v)
-        self.assertTrue(abs(a - 1.0) < 1.0e-05)
+    # def test_convert_to_word_vector(self):
+    #     v = TestDataPreprocessorForDevise.preprocessor.convert_to_word_vector(98)
+    #     self.assertTrue(v.shape == (200,))
+    #     a = np.linalg.norm(v)
+    #     self.assertTrue(abs(a - 1.0) < 1.0e-05)
 
-    def test_load_labels(self):
-        path = '/home/ubuntu/data/devise/selected_images_256_greater_than_200_images/label_selected.txt'
-        label2word = DataPreprocessorForDevise.load_labels(path)
-        self.assertTrue(label2word[98] == 'wringer')
+    # def test_load_labels(self):
+    #     path = '/home/ubuntu/data/devise/selected_images_256_greater_than_200_images/label_selected.txt'
+    #     label2word = DataPreprocessorForDevise.load_labels(path)
+    #     self.assertTrue(label2word[98] == 'wringer')
 
-    def test_get_example(self):
+    # def test_get_example(self):
+    #     preprocessor = TestDataPreprocessorForDevise.preprocessor
+    #     feature, w2v = preprocessor.get_example(0)
+    #     self.assertTrue(feature.shape == (1, 4096))
+    #     self.assertTrue(w2v.shape == (200,))
+
+    def test_iter(self):
         preprocessor = TestDataPreprocessorForDevise.preprocessor
-        feature, w2v = preprocessor.get_example(0)
-        self.assertTrue(feature.shape == (1, 4096))
-        self.assertTrue(w2v.shape == (200,))
+        batch_size = 3
+        iterator = chainer.iterators.SerialIterator(preprocessor, batch_size, shuffle=False)
+        print('===')
+        rs = iterator.next()
+        self.assertTrue(type(rs) == list)
+        self.assertTrue(len(rs) == batch_size)
+        for r in rs:
+            self.assertTrue(len(r) == 2)
+            self.assertTrue(type(r[0].data) == chainer.cuda.ndarray)
+            self.assertTrue(type(r[1]) == np.ndarray)
+            self.assertTrue(r[0].data.shape == (1, 4096))
+            self.assertTrue(r[1].shape == (200,))
+
+        # ダミーモデルを作って引数として何が入るのかを見極める.
 
 
 if __name__ == '__main__':
